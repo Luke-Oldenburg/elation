efe30c86671c2f4180e30b00c30a01367685dc1d
Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
2024-05-20 20:52:07 -0400

    Fix compare function

--- src/main.rs
+++ src/main.rs
@@ -19,8 +19,8 @@ fn main() -> Result<(), Box<dyn Error>> {
 
         let line: Vec<&str> = instructions[line_num].split(" ").collect();
         if line[0] == "compare" {
-            let argument1: i32 = line[1].parse().unwrap();
-            let argument2: i32 = line[3].parse().unwrap();
+            let argument1: i32 = variables.get(line[1]).expect("REASON").parse().unwrap();
+            let argument2: i32 = variables.get(line[3]).expect("REASON").parse().unwrap();
             if line[2] == "=" {
                 variables.insert(line[4], (argument1 ==argument2).to_string());
 
